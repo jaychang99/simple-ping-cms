a9ec23dc437871f96953fbc65e630645d5706a00
jaychang99 <jaychang99@gmail.com>
2023-09-30 16:17:51 +0900

    fix(SEO): og 데이터는 _document.tsx 에 넣기

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,15 +22,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="googlebot" content="noindex, nofollow" />
-        <meta name="robots" content="noindex, nofollow" />
-        <meta name="og:title" content="SCG Status CMS" />
-        <meta
-          name="og:description"
-          content="SCG status CMS 에 오신 것을 환영합니다. "
-        />
-        <meta name="og:image" content="/images/seo/og_image.png" />
         <title>SCG Status CMS</title>
       </Head>
       <SWRConfig
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="googlebot" content="noindex, nofollow" />
+        <meta name="robots" content="noindex, nofollow" />
+        <meta name="og:title" content="SCG Status CMS" />
+        <meta
+          name="og:description"
+          content="SCG status CMS 에 오신 것을 환영합니다. "
+        />
+        <meta name="og:image" content="/images/seo/og_image.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }

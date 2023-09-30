22b9fec9c656c7e32443ca42a13df044d765a0a4
jaychang99 <jaychang99@gmail.com>
2023-09-30 13:26:48 +0900

    feat(Service): 로그포함 서비스 목록 받을 때 날짜별로 조회하도록 설정

--- src/feature/dashboard/hooks/useGetServiceWithLogs.ts
+++ src/feature/dashboard/hooks/useGetServiceWithLogs.ts
@@ -1,13 +1,23 @@
 import { useConditionalSWR } from 'hooks/useConditionalSWR';
 import { ServiceWithLogs } from 'types/api/service';
 
+export type TUseGetServiceWithLogsFetcherArg = {
+  startDate: string;
+  endDate: string;
+} & {
+  shouldFetch: boolean;
+};
+
 /**
  * @description 로그를 포함한 서비스 목록 조회
  */
-const useGetServiceWithLogs = ({ shouldFetch = true }) => {
+const useGetServiceWithLogs = (args: TUseGetServiceWithLogsFetcherArg) => {
   return useConditionalSWR<ServiceWithLogs[]>(
-    { url: '/services/logs' },
-    shouldFetch,
+    {
+      url: '/services/logs',
+      query: { startDate: args.startDate, endDate: args.endDate },
+    },
+    args.shouldFetch,
   );
 };
 
--- src/feature/dashboard/views/ViewDashboard.tsx
+++ src/feature/dashboard/views/ViewDashboard.tsx
@@ -1,15 +1,66 @@
 import styled from '@emotion/styled';
+import { Button, DatePicker, Form } from 'antd';
+import dayjs from 'dayjs';
 import ServiceMetric from 'feature/dashboard/components/ServiceMetric';
 import useGetServiceWithLogs from 'feature/dashboard/hooks/useGetServiceWithLogs';
+import { useState } from 'react';
 
 interface Props {}
 
+type TFormValues = {
+  dateRange: [dayjs.Dayjs, dayjs.Dayjs];
+};
+
 const ViewDashboard = ({}: Props) => {
-  const { data } = useGetServiceWithLogs({});
+  const [searchParams, setSearchParams] = useState({
+    startDate: dayjs().subtract(1, 'day'),
+    endDate: dayjs(),
+  });
+  const { data } = useGetServiceWithLogs({
+    startDate: searchParams.startDate.format('YYYY-MM-DD'),
+    endDate: searchParams.endDate.format('YYYY-MM-DD'),
+    shouldFetch: true,
+  });
+
+  const handleFinish = (values: TFormValues) => {
+    const [startDate, endDate] = values.dateRange;
+
+    setSearchParams((searchParams) => {
+      return {
+        ...searchParams,
+        startDate,
+        endDate,
+      };
+    });
+  };
 
   return (
     <EmotionWrapper>
       <h3>서비스 지표</h3>
+      <Form
+        layout="inline"
+        onFinish={handleFinish}
+        initialValues={{
+          dateRange: [searchParams.startDate, searchParams.endDate],
+        }}
+      >
+        <div className="search-form-wrapper">
+          <Form.Item
+            name="dateRange"
+            rules={[
+              {
+                required: true,
+                message: '날짜를 입력해주세요',
+              },
+            ]}
+          >
+            <DatePicker.RangePicker />
+          </Form.Item>
+          <Button type="primary" htmlType="submit">
+            조회
+          </Button>
+        </div>
+      </Form>
       <div className="metric-container">
         {data?.map((item) => (
           <ServiceMetric key={item.uuid} {...item} />
@@ -33,5 +84,11 @@ const EmotionWrapper = styled.div`
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     grid-gap: 16px;
+  }
+
+  .search-form-wrapper {
+    display: flex;
+    margin-bottom: 16px;
+    align-items: flex-start;
   }
 `;

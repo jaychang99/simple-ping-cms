016a2e706041c9a1aaac90cb7971a61e578205ae
jaychang99 <jaychang99@gmail.com>
2023-10-01 14:00:23 +0900

    feat(Sidebar): 모바일  뷰에서는 사이드바 숨김

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -2,8 +2,9 @@ import React, { ReactNode } from 'react';
 import { Layout, Space } from 'antd';
 import UserProfile from 'components/layout/components/UserProfile';
 import MenuList from 'components/layout/components/MenuList';
+import styled from '@emotion/styled';
 
-const { Sider, Content } = Layout;
+const { Content } = Layout;
 
 const contentStyle: React.CSSProperties = {
   minHeight: '100vh',
@@ -11,24 +12,29 @@ const contentStyle: React.CSSProperties = {
   backgroundColor: '#eee',
 };
 
-const siderStyle: React.CSSProperties = {
-  textAlign: 'center',
-  lineHeight: '120px',
-  backgroundColor: '#fff',
-};
-
 type Props = {
   children: ReactNode;
 };
 
 export const AppLayout: React.FC<Props> = ({ children }) => (
-  <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
-    <Layout hasSider>
-      <div style={{ padding: 32 }}>
-        <UserProfile />
-        <MenuList />
-      </div>
-      <Content style={contentStyle}>{children}</Content>
-    </Layout>
-  </Space>
+  <EmotionWrapper>
+    <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
+      <Layout hasSider>
+        <div style={{ padding: 32 }} className="sidebar">
+          <UserProfile />
+          <MenuList />
+        </div>
+        <Content style={contentStyle}>{children}</Content>
+      </Layout>
+    </Space>
+  </EmotionWrapper>
 );
+
+const EmotionWrapper = styled.div`
+  .sidebar {
+    ${({ theme }) => theme.device.mobile} {
+      display: none;
+    }
+    padding: 32px;
+  }
+`;
